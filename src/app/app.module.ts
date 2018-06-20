16cd4930aa31e79d0b0561307ef26d4e6f6baa08
Raghvendra <itzraghu1@gmail.com>
2018-06-20 13:24:26 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,36 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { UniqueDeviceID } from '@ionic-native/unique-device-id';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ResultPage } from '../pages/result/result';
+
+
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+  MyApp,
+  HomePage,
+  ResultPage
   ],
   imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
+  BrowserModule,
+  IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+  MyApp,
+  HomePage,
+  ResultPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  StatusBar,
+  QRScanner,
+  UniqueDeviceID,
+  SplashScreen,
+  {provide: ErrorHandler, useClass: IonicErrorHandler},
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,63 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
+import { UniqueDeviceID } from '@ionic-native/unique-device-id';
+import { ResultPage } from '../result/result';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	scanSub: any;
+	constructor(
 
-  constructor(public navCtrl: NavController) {
+		public navCtrl: NavController,
+		private qrScanner: QRScanner,
+		private uniqueDeviceID: UniqueDeviceID
+		) {
+		
+	}
+	showCamera() {
+		(window.document.querySelector('ion-app') as HTMLElement).classList.add('cameraView');
+	}
 
-  }
+	hideCamera() {
+		(window.document.querySelector('ion-app') as HTMLElement).classList.remove('cameraView');
+	}
+
+	ionViewWillEnter(){
+
+		// get unique id
+		this.uniqueDeviceID.get()
+		.then((uuid: any) => console.log(uuid))
+		.catch((error: any) => console.log(error));
+
+
+		this.showCamera();
+
+		this.qrScanner.prepare()
+		.then((status: QRScannerStatus) => {
+			if (status.authorized) {
+				console.log('Camera Permission Given');
+				this.scanSub = this.qrScanner.scan().subscribe((text: string) => {
+					this.navCtrl.push(ResultPage,{'result':text});
+					this.qrScanner.hide();
+					this.scanSub.unsubscribe(); 
+				});
+				this.qrScanner.show();
+			} else if (status.denied) {
+				console.log('Camera permission denied');
+			} else {
+				console.log('Permission denied for this runtime.');
+			}
+		})
+		.catch((e: any) => console.log('Error is', e));
+	}
+	ionViewWillLeave(){
+		this.hideCamera(); 
+	}
+
+
 
 }
